3b68a110af84f86d37e88c92d360aeda01411d3c
Ayden Kranz <ayden.kranz@gmail.com>
2025-08-26 14:54:35 -0400

    AHHHHHHH

--- include/wc/input.h
+++ include/wc/input.h
@@ -1,10 +1,10 @@
 #include <libevdev-1.0/libevdev/libevdev.h>
 
-#define T wcinput_dev_list, struct libevdev*
+#define T wcinput_dev_vec, struct libevdev*
 #include "wctl/vector.h"
 
-#define T wcinput_ev_list, struct input_event
-#include "wctl/vector.h"
+#define T wcinput_ev_que, struct input_event
+#include "wctl/queue.h"
 
 typedef struct wcinput_device_s{
     struct libevdev* dev;
@@ -17,6 +17,17 @@ typedef struct wcinput_event_s{
 } wcinput_event;
 
 typedef struct wcinput_ctx_s{
-    wcinput_dev_list devices;
-    wcinput_ev_list events;
+    wcinput_dev_vec devices;
+    wcinput_ev_que events;
 } wcinput_ctx;
+
+int wcinput_ctx_init(wcinput_ctx* ctx){
+    if (wcinput_dev_vec_init(&ctx->devices, 4)) return -1;
+    if (wcinput_ev_que_init(&ctx->events, 8)) return -1;
+    return 0;
+}
+int wcinput_ctx_find_devices(wcinput_ctx* ctx){
+    return 0;
+}
+
+
